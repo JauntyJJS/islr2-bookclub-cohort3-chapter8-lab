01758199e93ee5a0b00f7bccacdcb0adfff4bb8e
JauntyJJS <jeremy1189.jjs@gmail.com>
2022-04-17 23:46:45 +0800

    Initial commit

---
+++ utils/render.R
@@ -0,0 +1,12 @@
+suppressWarnings(library(rmarkdown))
+
+# Try to find the correct working dir
+if(!file.exists("index.Rmd") && file.exists("../index.Rmd")) {
+  setwd("../")
+}
+
+# Render HTML
+rmarkdown::render('index.Rmd',
+                  output_format = "html_document",
+                  output_dir = "docs/",
+                  output_file = "index")
